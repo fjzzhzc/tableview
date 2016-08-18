9590b835eef20219517596dbd8954e27ebae38b7
fjzzhzc <huangzc.it@gmail.com>
2016-08-18 19:46:59 +0900

    comment out something

--- tableview/NetVC.m
+++ tableview/NetVC.m
@@ -7,6 +7,10 @@
 //
 
 #import "NetVC.h"
+#import "AFNetworking.h"
+#import "AFHTTPSessionManager.h"
+//#import <AVFoundation/AVFoundation.h>
+
 
 @interface NetVC ()
 
@@ -19,11 +23,38 @@
     self.view.backgroundColor = [UIColor colorWithRed:0.3 green:0.6 blue:0.2 alpha:0.5];
     _netData = [[NSMutableData alloc] init];
     
-    NSString *str = @"http://baidu.cn";
-    NSURL *url = [NSURL URLWithString:str];
-    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:url];
+//    NSString *str = @"http://baidu.cn";
+//    NSURL *url = [NSURL URLWithString:str];
+//    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:url];
     
-    NSURLConnection *con = [NSURLConnection connectionWithRequest:req delegate:self];
+//    NSURLConnection *con = [NSURLConnection connectionWithRequest:req delegate:self];
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+//    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
+    
+//    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+//       
+//        switch(status){
+//            case AFNetworkReachabilityStatusUnknown:NSLog(@"reachabilitystatus:UNKNOWN");break;
+//            case AFNetworkReachabilityStatusNotReachable:NSLog(@"reachabilitystatus:NotReachable");break;
+//            case AFNetworkReachabilityStatusReachableViaWWAN:NSLog(@"reachabilitystatus:WWAN");break;
+//            case AFNetworkReachabilityStatusReachableViaWiFi:NSLog(@"reachabilitystatus:WIFI");break;
+//        }
+//        
+//    }];
+    
+    AFHTTPSessionManager *sessionManager = [AFHTTPSessionManager manager];
+    sessionManager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    sessionManager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+    [sessionManager GET:@"http://baidu.cn" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"success:%@",responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"fail:%@",error);
+    }];
+
 }
 
 -(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
